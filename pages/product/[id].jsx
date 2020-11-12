d6327a8d201c91360d4f4862265363c9b4fa7c2c
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-11-12 17:51:06 -0600

    Id ProducPage component functionality take url id and link to product/id and styles

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router'
 import Card from 'react-bootstrap/Card'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import styles from '../product/id.module.css'
 
 const ProductPage = () => {
   const { query } = useRouter()
@@ -23,17 +24,34 @@ const ProductPage = () => {
     <section>
       <h1 className="p-5">Página producto: {query.id}</h1>
       <Row>
-        <Col>
-          <Card style={{ width: '18rem' }}>
+        <Col lg={4} style={{ paddingLeft: '0px', paddingRight: '0px' }}>
+          <Card /* style={{ width: '18rem' }} */>
             <Card.Img variant="top" src={avocado.image} />
           </Card>
+        </Col>
+        <Col
+          lg={8}
+          style={{
+            paddingLeft: '0px',
+            paddingRight: '0px',
+            display: 'flex',
+            justifyContent: 'end',
+          }}
+        >
           <Card>
             <Card.Body>
-              <Card.Title>{avocado.name}</Card.Title>
-              <Card.Text>{avocado.attributes.description}</Card.Text>
+              <Card.Title className="text-info">
+                {avocado.name}{' '}
+                <span className="text-secondary">ID: {avocado.id}</span>
+              </Card.Title>
+              <Card.Text>
+                {/* {avocado.attributes.description} */} Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Animi totam error placeat?
+                Ratione placeat voluptatibus omnis quos, possimus et provident.
+              </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">{avocado.price}</small>
+              <small className="text-success">$ {avocado.price}</small>
             </Card.Footer>
           </Card>
         </Col>
