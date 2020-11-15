e2ffede868732372f67c82455257f409af373ddd
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-11-14 20:56:27 -0600

    fix property attributs in data for product id

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -11,22 +11,17 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
 import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import Dropdown from 'react-bootstrap/Dropdown'
 
 const ProductPage = () => {
   const [avocado, setAvocado] = useState({})
   const [loading, setLoading] = useState(true)
   const [quantity, setQuantity] = useState(1)
 
-  const { name, sku, price, image, attributes } = avocado
-
-  //const { shape, hardiness, taste } = attributes
-
   const {
     query: { id },
   } = useRouter()
+
+  const { name, sku, price, image, attributes } = avocado
 
   useEffect(() => {
     setLoading(true)
@@ -61,7 +56,7 @@ const ProductPage = () => {
           <Card.Title>
             <small className="text-success">$ {price}</small>
           </Card.Title>
-          <Button variant="secondary">sku: {id}</Button>
+          <Button variant="secondary">sku: {sku}</Button>
 
           <Row>
             <InputGroup className="mb-3 mt-3 pl-3">
@@ -86,15 +81,15 @@ const ProductPage = () => {
         <tbody>
           <tr>
             <td>Shape</td>
-            {/* <td>{shape}</td> */}
+            <td>{attributes.shape}</td>
           </tr>
           <tr>
             <td>Hardiness</td>
-            {/* <td>{hardiness}</td> */}
+            <td>{attributes.hardiness}</td>
           </tr>
           <tr>
             <td>Taste</td>
-            {/*  <td>{taste}</td> */}
+            <td>{attributes.taste}</td>
           </tr>
         </tbody>
       </Table>
