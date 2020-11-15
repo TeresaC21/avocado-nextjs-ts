efacedfb755cc6b10bfc1bfb501961bd24f6cf3b
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-11-14 22:42:30 -0600

    Navbar component code clean

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -30,15 +30,5 @@ export default function Navbar() {
         </Link>
       </Container>
     </NavbarBt>
-    /*  <nav>
-      <menu>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </menu>
-    </nav> */
   )
 }
